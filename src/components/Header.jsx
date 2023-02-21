77289c9c9269fded65b1f07aad51a7e61febbd30
Nkaleth <nseguralu@gmail.com>
2023-02-20 20:22:06 -0500

    Add styling to the header and todoItem components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,15 @@
+import styles from "styles/Header.module.css";
+
+
 const Header = () => {
+  const headerstyle= {
+    padding: '20px 0',
+    lineHeight: '1.5em',
+    color: '#aeadad',
+    textAlign: 'center',
+  };
   return (
-    <header>
+    <header style={headerstyle} className={styles.header}>
       <h1>todos</h1>
       <p>Items will persist in the browser local storage</p>
     </header>
