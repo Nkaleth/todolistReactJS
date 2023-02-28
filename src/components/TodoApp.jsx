364ff7286fae49f5c22846db77dea479b85b0461
Nkaleth <nseguralu@gmail.com>
2023-02-28 10:01:14 -0500

    Add routers for home, About, Profile, Login

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,15 +1,21 @@
-import Header from 'components/Header';
-import TodosLogic from 'components/TodosLogic';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from 'routes/Home';
+import About from 'routes/About';
+import Login from 'routes/Login';
+import Profile from 'routes/Profile';
+import NotMatch from 'routes/NotMatch';
+import Layout from 'components/Layout';
 
 const TodoApp = () => (
-  <div className="wrapper">
-    {' '}
-    <div className="todos">
-      {' '}
-      <Header />
-      {' '}
-      <TodosLogic />
-    </div>
-  </div>
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="login" element={<Login />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="*" element={<NotMatch />} />
+    </Route>
+  </Routes>
 );
 export default TodoApp;
